ad92a556b575be6cbc7dc88a7026a32e09243107
yoonhee kim <mini.yoonhee@gmail.com>
2019-03-13 21:29:48 -0400

    associate review table and user table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,10 +29,13 @@ module.exports = function(app) {
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    console.log(res)
+    console.log("res.body : "+res);
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
+    // res.json("/", {
+    //   userDB : res.body
+    // });
     res.json("/members");
   });
 
